cc4fed0205d5fe09c67a5d4a5026f3559b420ed2
borzenkoelizaveta <66558867+borzenkoelizaveta@users.noreply.github.com>
2020-06-07 18:10:32 +0300

    I've checked all previous parts. I've made an input part about the goal of the ration. I've made an output part that gives the user his ration according to the input data.

--- part2.py
+++ part2.py
@@ -1,4 +1,5 @@
 import os
+
 times = 3
 max_comb = 600
 form = 1
@@ -15,6 +16,7 @@ physical_activity = 2
 not_food = 'Fish,;;;:   miLK'
 chosen_option = 1
 
+
 def less(a, b):
     global days
     global allergy_free
@@ -29,6 +31,7 @@ def less(a, b):
         return True
     else:
         return False
+
 
 def init():
     global PATH
@@ -56,8 +59,8 @@ def init():
         for j in range(0, int(s[2])):
             recipes[i][2][j] = f.readline().split()[0]
 
-
     f.close()
+
 
 def create_days():
     global allergy_free
@@ -81,6 +84,7 @@ def create_days():
                     # if len(days_local) >= max_comb:
                     # break
     return days_local
+
 
 def my_sort(data):
     for i in range(0, len(data)):
@@ -111,6 +115,7 @@ def create_chosen_days(days):
             break
     return chosen_days
 
+
 def is_letter(x):
     if ord(x) >= ord('a') and ord(x) <= ord('z'):
         return 1
@@ -119,8 +124,8 @@ def is_letter(x):
     else:
         return 3
 
-def capitalize(data):
 
+def capitalize(data):
     data = data.replace('.', ' ')
     data = data.replace(',', ' ')
     data = data.replace(';', ' ')
@@ -167,15 +172,15 @@ def fill_in_the_form():
     print("Print 2 if you want to maintain your body weight")
     print("Print 3 if you want to gain weight")
     chosen_option_local = int(input())
-    return [height_local, weight_local, gender_local, age_local, physical_activity_local, not_food_local, chosen_option_local]
-
+    return [height_local, weight_local, gender_local, age_local, physical_activity_local, not_food_local,
+            chosen_option_local]
 
 
 finish = False
 while form == 1 and finish == False:
     try:
-       personal_data = fill_in_the_form()
-       finish = True
+        personal_data = fill_in_the_form()
+        finish = True
     except:
         print('Data is incorrect, try again')
         finish = False
@@ -189,4 +194,94 @@ if form == 1:
     chosen_option = personal_data[6]
 not_food = capitalize(not_food)
 
+BM = 10 * weight + 6.25 * height + 5 * age + 5
+if gender == 'female':
+    BM -= 156
+DCI = BM * 1.2
+if physical_activity == 1:
+    DCI = BM * 1.2
+elif physical_activity == 2:
+    DCI = BM * 1.375
+elif physical_activity == 3:
+    DCI = BM * 1.55
 
+if chosen_option == 1:
+    DCI = DCI - 200
+elif chosen_option == 3:
+    DCI = DCI + 200
+DCI = int(DCI)
+
+init()
+
+for i in range(0, len(recipes)):
+    cnt = 0
+    for j in range(0, len(recipes[i][2])):
+        if recipes[i][2][j] in not_food:
+            cnt = 1
+    if cnt == 0:
+        allergy_free.append(recipes[i])
+
+days = create_days()
+
+print('Please wait, sorting in progress')
+days = my_sort(days)
+print('Sorting finished')
+chosen_days = create_chosen_days(days)
+print('Chosen days finished')
+
+while True:
+    print('Print 1 for all available recipes')
+    print('Print 2 for allergy free recipes')
+    print('Print 3 for recommended daily calorie input')
+    print("Print 4 for recommended nutrition program for a week")
+    print("Print 5 for exit")
+    option = int(input())
+
+    if option == 1:
+        for i in range(0, len(recipes)):
+            print(recipes[i][0], recipes[i][1])
+            for j in range(0, len(recipes[i][2])):
+                print(recipes[i][2][j], end=" ")
+            print()
+            print()
+
+    elif option == 2:
+        for i in range(0, len(allergy_free)):
+            print(allergy_free[i][0], allergy_free[i][1])
+            for j in range(0, len(allergy_free[i][2])):
+                print(allergy_free[i][2][j], end=" ")
+            print()
+            print()
+
+    elif option == 3:
+        print(DCI)
+        print()
+
+    elif option == 4:
+        x = len(chosen_days)
+        if x == 0:
+            print('Unfortunately, no such foods available')
+            continue
+        for i in range(0, 7):
+            i0 = chosen_days[i % x][0]
+            i1 = chosen_days[i % x][1]
+            i2 = chosen_days[i % x][2]
+            print('Day', i + 1)
+            print()
+            print('Breakfast:', end=" ")
+            print(allergy_free[i0][0], allergy_free[i0][1])
+            if len(chosen_days[i % x]) > 3:
+                print('Brunch:', end=" ")
+                print(allergy_free[chosen_days[i % x][3]][0], allergy_free[chosen_days[i % x][3]][1])
+            print('Lunch:', end=" ")
+            print(allergy_free[i1][0], allergy_free[i1][1])
+            print('Diner:', end=" ")
+            print(allergy_free[i2][0], allergy_free[i2][1])
+            if len(chosen_days[i % x]) > 4:
+                print('Second dinner:', end=" ")
+                print(allergy_free[chosen_days[i % x][4]][0], allergy_free[chosen_days[i % x][4]][1])
+            print()
+
+    elif option == 5:
+        break
+       
